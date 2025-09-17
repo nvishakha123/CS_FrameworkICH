73695e2d776e8a05455d85a3ca13e6bf0fabaa4a
nvishakha123 <vishakha.nandanwar@kreedalabs.com>
2025-09-17 17:31:19 +0530

    add lib

--- src/test/java/com/fashion/testcases/TC_LoginPage.java
+++ src/test/java/com/fashion/testcases/TC_LoginPage.java
@@ -14,14 +14,17 @@ public class TC_LoginPage extends BaseClass{
 		//open url
 		driver.get(url);
 		logger.info("url opened");
+		System.out.println("Hello from test");
 		Thread.sleep(100);
 		LogincicdPage logincicdPg = new LogincicdPage(driver);
 		logincicdPg.enterUserName("vishakha.nandanwar@kreedalabs.com");
 		logger.info("username entered");
+		System.out.println("Hello from test1");
 		logincicdPg.enterPassword("Password@3");
 		logger.info("password entered");
 		logincicdPg.login_btn();
 		logger.info("login button clicked");
+		System.out.println("Hello from test2");
 		Thread.sleep(800);
 	}
 	
